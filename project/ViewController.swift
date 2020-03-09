0226f1b6efbe510bcba3327c2d97733aae9cea24
danil <danil.ejow98@gmail.com>
2020-03-09 15:02:19 +0300

    Initial Commit

---
+++ project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  project
+//
+//  Created by Данил Ежов on 09.03.2020.
+//  Copyright © 2020 Данил Ежов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
